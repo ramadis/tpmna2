a236a9a23d33b378f49a0e80085b91bc9cc061ae
lulitay <ltay@itba.edu.ar>
2019-11-08 12:27:51 -0300

    Added custom implementation of fast fourier transform

---
+++ CustomFft.m
@@ -0,0 +1,15 @@
+function y = CustomFft(x)
+n = length(x);
+
+if mod(n,2) == 0
+    dom = (0:n/2-1);
+    e = exp(-2i*pi/n) .^ dom;
+    odd = custom_fft(x(1:2:n-1));
+    even = e.*custom_fft(x(2:2:n));
+    y = [odd+even, odd-even];
+else
+    dom_t = 0:n-1;
+    dom = dom_t';
+    F = exp(-2i*pi/n) .^ (dom*dom_t);
+    y = F*x;
+end
