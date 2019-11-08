102fd463b9544cc7e74894d2f7e08e797e6d33b3
ramadis <rramiro.o@hotmail.com>
2019-11-08 17:31:44 -0300

    Add Difference function, to improve plotting later

---
+++ Difference.m
@@ -0,0 +1,6 @@
+function D = Difference(ua, ub)
+  % This function calculates the difference, given 2 discretized functions
+  
+  D = ua - ub;
+
+end
--- Error.m
+++ Error.m
@@ -1,6 +1,6 @@
 function E = Error(ua, ub)
   % This function calculates the error, given 2 discretized functions
   
-  E = norm(ua - ub, "inf");
+  E = norm(Difference(ua, ub), "inf");
 
 end
