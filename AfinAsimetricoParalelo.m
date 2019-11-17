77a625f352a55f915962c8036a353b00cd000da6
jjant <jantonielli@itba.edu.ar>
2019-11-16 23:23:23 -0300

    Fix affine asymetric parallel thing?

--- AfinAsimetricoParalelo.m
+++ AfinAsimetricoParalelo.m
@@ -1,25 +1,21 @@
 function U = AfinAsimetricoParalelo(h, U, k, q)
-  % This function calculates the next U using
-  % Afin integrator asymmetric with parallel computing.
+% This function calculates the next U using
+% Afin integrator asymmetric with parallel computing.
 
-  nucleus = 3
-  x = U
-  gammas = GamasAsimetrico(q);
-  spmd(nucleus)
-    for j = 1:q
-      if labindex == s
-        for s = 1:labindex
-          x = AfinMas(h/labindex, x, k);
-        end
-      end
-      x = gammas(labindex) * x;
-    end
-  end
-  
-  for s = 2:q
-    x{1} = x{1}+x{s};
-  end
+x = U;
+gammas = GamasAsimetrico(q);
 
-  U = x{1};
+spmd(q)
+    for j = 1:labindex
+        x = AfinMas(h/labindex, x, k);
+    end 
+    x = gammas(labindex) * x;
+end
+
+for s = 2:q
+    x{1} = x{1} +x{s};
+end
+
+U = x{1};
 
 end
